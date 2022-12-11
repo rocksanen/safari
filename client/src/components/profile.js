25caeac5a99f8769b0fd0c5462b373ee7038fa17
mohammja <mohammja@metropolia.fi>
2022-12-11 21:33:07 +0200

    profile component done!

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 const Profile = ({user}) => {
   
     const [open, setOpen] =useState(false);
+    let total = 0;
 
     const handleOpen = () => {
       setOpen(!open);
@@ -29,13 +30,18 @@ const Profile = ({user}) => {
             </h4>
             <ul>
                 {user && user.orders.map((order)=>{
+                  total += order.price;
                     return(
                         <li key={order.id}>
                             {order.name} - {order.price} €
+                            
                         </li>
                     )
                 })}
             </ul>
+            <h4>
+              Total: {total} €
+            </h4>
         </div> : <div></div>}
       </div>
     );
